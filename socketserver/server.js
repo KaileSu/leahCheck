0b7fdceec308478efe7f3f4d7737ab4f91015c1c
Kaile Su <kailepku@gmail.com>
2020-10-07 12:27:12 +1000

    first

---
+++ socketserver/server.js
@@ -0,0 +1,50 @@
+// Get dependencies
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const MongoClient = require('mongodb').MongoClient;
+
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+
+// Parse Requests
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// Enable CORS for all HTTP methods
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, OPTIONS");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+const products = require('./dbOperations/operations'); // Define a sequence of db operations
+app.post('/productInsert', products.insert);
+app.get('/productFind', products.find);
+app.post('/productUpdate', products.update);
+app.post('/productDelete', products.delete);
+
+io.on('connection', function(socket) {
+    console.log('A user connected');
+
+    socket.emit('test event', 'new connection');
+
+    //New user joining a room
+    socket.on('join', function(data) {
+        socket.join(data.room);
+        console.log(data.user + ' joined the room: ' + data.room);
+        socket.broadcast.to(data.room).emit('new user joined', {user:data.user, message:' has joined this room.'});
+    });
+
+    //Existing user leaving a room
+    socket.on('leave', function(data) {
+        console.log(data.user + ' left the room: ' + data.room);
+        socket.broadcast.to(data.room).emit('left room', {user:data.user, message:' has left this room.'});
+        socket.leave(data.room);
+    });
+});
+
+server.listen(3000, () => {
+    console.log('Socket.io server is listening on port 3000');
+});
